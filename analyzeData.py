44fcc34e0d0cf77053c69d27efeaa22ebedd1451
evandropp10 <evandropp10@gmail.com>
2019-04-15 14:06:37 -0300

    commit

--- analyzeData.py
+++ analyzeData.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pymongo
 
-def analyze():
+def analyzeGoogle():
     #client = pymongo.MongoClient('mongodb://localhost:27017/')
     client = pymongo.MongoClient('mongodb://datastore:27017/')
     db = client['database']
@@ -14,7 +14,7 @@ def analyze():
     # 1.1
     df['domain'] = df['link'].apply(lambda x: x.split('/')[2])
     dfDomains = pd.DataFrame(df['domain'].value_counts())
-    dfDomains.to_csv('domains.csv')
+    dfDomains.to_csv('Google_domains.csv')
 
     ## 1.2
     df['content'] = df['content'].str.lower()
@@ -32,10 +32,47 @@ def analyze():
 
     # Gerando os 100 primeiros termos com mais incidência
     df100 = pd.DataFrame(dfNew['words'].value_counts().head(100))
-    df100.to_csv('100.csv')
+    df100.to_csv('Google_100.csv')
+
+
+def analyzeTwitter():
+    #client = pymongo.MongoClient('mongodb://localhost:27017/')
+    client = pymongo.MongoClient('mongodb://datastore:27017/')
+    db = client['database']
+    coll = db['twitter']
+
+    cursor = coll.find()
+
+    df = pd.DataFrame(list(cursor))
+
+    #2.1
+    dfUser = pd.Dataframe(df['user'].value_counts())
+    dfUser.to_csv('Twitter_users.csv')
+
+    #2.2
+    df['weekday'] = df['datetime'].apply(lambda x: x.isoweekday())
+    dias = [
+        'Segunda-feira',
+        'Terça-feira',
+        'Quarta-feira',
+        'Quinta-Feira',
+        'Sexta-feira',
+        'Sábado',
+        'Domingo'
+    ]
+    df['weekday_name'] = df['weekday'].apply(lambda x: dias[x-1])
+    dfDay = pd.DataFrame(df['weekday_name'].value_counts())
+    dfDay.to_csv('Twitter_dayweek.csv')
+
+    #2.3
+    df['hour'] = df['datetime'].apply(lambda x: x.hour)
+    dfHour = pd.DataFrame(df['hour'].value_counts())
+    dfHour.to_csv('Twitter_hour.csv')
+
 
 
 '''
+TEST MONGO
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client['telemedicina']
 coll = db['google_rank']
--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import scraper
-import analyzeGoogleData
+import analyzeData
 import extractorTwitter
 
 
 def main():
+    
     print('>>> Scraping GOOGLE...')
     scraper.scrapGoogle('telemedicina')
 
-    print('>>> Analyzing Data...')
-    analyzeGoogleData.analyze()
+    print('>>> Analyzing Google Data...')
+    analyzeData.analyzeGoogle()
     print('>>> FINISH. Please check 100.csv and domains.csv')
-
+    
     print('>>> Extracting Twitter Data...')
     extractorTwitter.queryTwitter('telemedicina')
+    
+    print('>>> Analyzing Twitter Data...')
+    analyzeData.analyzeTwitter()
+    print('>>> FINISH. Please check Google_100.csv, Google_domains.csv, Twitter_users.csv, Twitter_dayweek.csv and Twitter_hour.csv')
+    
 
 if __name__ == '__main__':
     main()
