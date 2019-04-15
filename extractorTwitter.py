79b40c459baced347284d803c11cf03a2f6bfcbe
evandropp10 <evandropp10@gmail.com>
2019-04-15 08:08:01 -0300

    Update extractorTwitter.py

--- extractorTwitter.py
+++ extractorTwitter.py
@@ -12,15 +12,33 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 #public_tweets = api.search('bitcoin since:2012-01-01 until:2014-01-30') #, lang='en', rpp=100)
-public_tweets = api.search('telemedicina')
+public_tweets = api.search(q='telemedicina', count=100)
 public_tweets
 
-for tweet in public_tweets:
+cont = 0
+lastId = 0
+i = 0
+while i <= 10: 
+    if i >= 1:
+        public_tweets = api.search(q='telemedicina', count=100, max_id=lastId)
+    for tweet in public_tweets:
+        
+        # atw.append(tweet.id)
+        # atw.append(tweet.created_at)
+        # atw.append(tweet.user.id)
+        # atw.append(tweet.text)
+        print('--------------------')
+        print(tweet.id)
+        print(tweet.created_at)
+        print(tweet.user.id)
+        print(tweet.user.screen_name)
+        print(tweet.text)
+        print(cont)
+        lastId = tweet.id
+
+        cont += 1
+        if cont >= 1000:
+            break
+        
     
-    # atw.append(tweet.id)
-    # atw.append(tweet.created_at)
-    # atw.append(tweet.user.id)
-    # atw.append(tweet.text)
-    
-    print(tweet.text)
-    
+    i += 1
